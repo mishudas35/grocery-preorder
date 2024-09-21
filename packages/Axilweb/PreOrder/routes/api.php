a6f8ecc8090805687fec7e8736f52e3f32fdbaa7
Mishu Das <mishu.das35bng@gmail.com>
2024-09-21 13:29:14 +0600

    pre order list api

--- packages/Axilweb/PreOrder/routes/api.php
+++ packages/Axilweb/PreOrder/routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('preorder')->group(function () {
     Route::post('/store', [PreOrderController::class, 'store']);
+    Route::get('/list', [PreOrderController::class, 'index']);
     Route::post('/product', [ProductController::class, 'store']);
     Route::get('/products', [ProductController::class, 'index']);
 });
--- packages/Axilweb/PreOrder/src/Controllers/PreOrderController.php
+++ packages/Axilweb/PreOrder/src/Controllers/PreOrderController.php
@@ -8,6 +8,31 @@ use Illuminate\Routing\Controller;
 
 class PreOrderController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        // Define the query for pre-orders
+        $query = PreOrder::query();
+
+        // Search by name or email if the search parameter is provided
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where('name', 'like', "%{$search}%")
+                ->orWhere('email', 'like', "%{$search}%");
+        }
+
+        // Apply ordering if the order_by parameter is provided (default to 'id')
+        $orderBy = $request->input('order_by', 'id');
+        $orderDirection = $request->input('order_direction', 'asc');
+        $query->orderBy($orderBy, $orderDirection);
+
+        // Paginate the results (default 10 per page)
+        $preOrders = $query->paginate($request->input('per_page', 10));
+
+        // Return the paginated results as a JSON response
+        return response()->json($preOrders);
+    }
+
     /**
      * Store a new pre-order.
      */
